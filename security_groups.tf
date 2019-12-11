6d85db6659ece0485307feb6b303b91fd8c7169b
Sam Jones <sam.jones34@nhs.net>
2019-12-11 16:55:30 +0000

    Fixed security groups

--- security_groups.tf
+++ security_groups.tf
@@ -37,13 +37,13 @@ resource "aws_security_group" "web-sg" {
         from_port = 22
         to_port = 22
         protocol = "ssh"
-        security_groups = [$"{aws_security_group.mgmt-sg.id}"]
+        security_groups = ["${aws_security_group.mgmt-sg.id}"]
     }
     ingress {
         from_port = -1
         to_port = -1
         protocol = "icmp"
-        security_groups = [$"{aws_security_group.mgmt-sg.id}"]
+        security_groups = ["${aws_security_group.mgmt-sg.id}"]
     }
     egress {
         from_port   = 80
@@ -61,7 +61,7 @@ resource "aws_security_group" "web-sg" {
         from_port   = 3306
         to_port     = 3306
         protocol    = "tcp"
-        security_groups = [$"{aws_security_group.dbsg.id}"]
+        security_groups = ["${aws_security_group.dbsg.id}"]
     }
 
     tags = {
@@ -85,19 +85,19 @@ resource "aws_security_group" "mgmt-sg" {
         from_port = 22
         to_port = 22
         protocol = "ssh"
-        security_groups = [${aws_security_group.web-sg.id}]
+        security_groups = ["${aws_security_group.web-sg.id}"]
     }
     egress {
         from_port = -1
         to_port = -1
         protocol = "icmp"
-        security_groups = [${aws_security_group.web-sg.id}]
+        security_groups = ["${aws_security_group.web-sg.id}"]
     }
     egress {
         from_port = 3306
         to_port = 3306
         protocol = "tcp"
-        security_groups = [${aws_security_group.dbsg.id}]
+        security_groups = ["${aws_security_group.dbsg.id}"]
     }
 
     tags = {
