486d4ea32670f874a51d7ecc42d4d5e13c9c4570
Sam Jones <sam.jones34@nhs.net>
2019-12-11 16:15:50 +0000

    Added bastion host

--- ec2_servers.tf
+++ ec2_servers.tf
@@ -31,3 +31,19 @@ resource "aws_instance" "webserver-2" {
         Terraform = true
     }
 }
+
+resource "aws_instance" "bastion" {
+    ami = "${data.aws_ami.aws_linux_ami.id}"
+    availability_zone = "${var.mgmt_subnet1}"
+    instance_type = "t3.micro"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.mgmt-sg.id}"]
+    subnet_id = "${aws_subnet.mgmt_subnet1.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+
+    tags = {
+        Name = "Bastion and Ansible Server"
+        Terraform = true
+    }
+}
