7fc7bbe060330a4284037911d4cd7a27fea417ae
samjones30 <43493065+samjones30@users.noreply.github.com>
2020-01-07 09:41:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,17 @@ provider "aws" {
   version     = "~> 2.7"
 }
 
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket          = "training-infra-tf-state-management"
-    key             = "global/s3/terraform.tfstate"
-    region          = "eu-west-2"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table  = "training-infra-tf-lock-management"
-    encrypt         = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    # Replace this with your bucket name!
+#    bucket          = "training-infra-tf-state-management"
+#    key             = "global/s3/terraform.tfstate"
+#    region          = "eu-west-2"
+#    # Replace this with your DynamoDB table name!
+#    dynamodb_table  = "training-infra-tf-lock-management"
+#    encrypt         = true
+#  }
+#}
 
 #########################
 ###Set up data sources###
